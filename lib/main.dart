d7bd1a2ca8ea837c0c30f2082840eaf6379b1d9f
Danaes <alvarez17dani@gmail.com>
2019-10-30 14:07:56 +0100

    Add ClipRect widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     SystemChrome.setSystemUIOverlayStyle( SystemUiOverlayStyle.light.copyWith(
-      statusBarColor: Colors.white12
+      statusBarColor: Colors.transparent
     ));
 
     return MaterialApp(
--- lib/src/pages/botones_page.dart
+++ lib/src/pages/botones_page.dart
@@ -1,4 +1,6 @@
 import 'dart:math';
+import 'dart:ui' as prefix0;
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -13,6 +15,7 @@ class BotonesPage extends StatelessWidget {
             child: Column(
               children: <Widget>[
                 _titulos(),
+                _botonesRedondeados()
               ],
             ),
           )
@@ -117,5 +120,68 @@ class BotonesPage extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Widget _botonesRedondeados() {
+
+    return Table(
+      children: [
+        TableRow(
+          children: [
+            _crearBotonRedondeado( Colors.blue, Icons.border_all, 'General'),
+            _crearBotonRedondeado( Colors.purpleAccent, Icons.directions_bus, 'Bus')
+          ]
+        ),
+        TableRow(
+          children: [
+            _crearBotonRedondeado( Colors.pinkAccent, Icons.shop, 'Buy'),
+            _crearBotonRedondeado( Colors.orange, Icons.insert_drive_file, 'File')
+          ]
+        ),
+        TableRow(
+          children: [
+            _crearBotonRedondeado( Colors.red, Icons.collections, 'Photos'),
+            _crearBotonRedondeado( Colors.teal, Icons.help_outline, 'Grocery')            
+          ]
+        ),
+        TableRow(
+          children: [
+            _crearBotonRedondeado( Colors.blueAccent, Icons.movie_filter, 'Enterteiment'),
+            _crearBotonRedondeado( Colors.green, Icons.cloud, 'Grocery')            
+          ]
+        )
+      ],
+    );
+  }
+
+  Widget _crearBotonRedondeado(Color color, IconData icono, String texto) {
+
+    return ClipRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur( sigmaX: 5.0, sigmaY: 5.0),
+        child: Container(
+          height: 160,
+          margin: EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20.0),
+            color: Color.fromRGBO(62, 66, 107, 0.7)
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              SizedBox( height: 5.0,),
+              CircleAvatar(
+                backgroundColor: color,
+                radius: 35.0,
+                child: Icon( icono, color: Colors.white, size: 30.0 ),
+              ),
+              Text(texto, style: TextStyle( color: color)),
+              SizedBox( height: 5.0,)
+            ],
+          ),
+        ),
+      )
+    );
+
   }
 }
